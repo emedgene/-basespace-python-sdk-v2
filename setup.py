c71eab93d8048b69b7c68e53536f8c74bce349f1
Shani-Daniel <153742552+Shani-Daniel@users.noreply.github.com>
2025-01-07 13:18:48 +0200

    updating version to 1.2.2 (#4)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "bssh_sdk_2"
-VERSION = "1.0.0"
+VERSION = "1.2.2"
 # To install the library, run the following
 #
 # python setup.py install
